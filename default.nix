0d200da557ff24957e91c598953d072bd35a9b38
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-03-05 18:27:25 +0100

    Fixed nix-shell

--- default.nix
+++ default.nix
@@ -1,32 +1,19 @@
-# This is an example of what downstream consumers of holonix should do
-# This is also used to dogfood as many commands as possible for holonix
-# For example the release process for holonix uses this file
-let
-
- # point this to your local config.nix file for this project
- # example.config.nix shows and documents a lot of the options
- config = import ./config.nix;
-
- # START HOLONIX IMPORT BOILERPLATE
- holonix = import (
-  if ! config.holonix.use-github
-  then config.holonix.local.path
-  else fetchTarball {
-   url = "https://github.com/${config.holonix.github.owner}/${config.holonix.github.repo}/tarball/${config.holonix.github.ref}";
-   sha256 = config.holonix.github.sha256;
-  }
- ) { config = config; };
- # END HOLONIX IMPORT BOILERPLATE
-
-in
-with holonix.pkgs;
-{
- dev-shell = stdenv.mkDerivation (holonix.shell // {
-  name = "dev-shell";
-
-  buildInputs = [ ]
-   ++ holonix.shell.buildInputs
-   ++ config.buildInputs
-  ;
- });
-}
+let 
+  holonixPath = builtins.fetchTarball {
+    url = "https://github.com/holochain/holonix/archive/cdf1d199d5489ebc943b88e552507f1063e3e571.tar.gz";
+    sha256 = "1b5pdlxj91syg1qqf42f49sxlq9qd3qnz7ccgdncjvhdfyricagh";
+  };
+  holonix = import (holonixPath) {
+    includeHolochainBinaries = true;
+    holochainVersionId = "custom";
+    
+    holochainVersion = { 
+     rev = "fd8049a48ac12ef3e190b48a79ffe8d8b5948caa";  
+     sha256 = "065kkmmr8b5ngjqpr7amd7l4dcakj2njx168qvr5z47mmqs9xbgw";  
+     cargoSha256 = "1ix8ihlizjsmx8xaaxknbl0wkyck3kc98spipx5alav8ln4wf46s";
+     bins = {
+       holochain = "holochain";
+     };
+    };
+  };
+in holonix.main
