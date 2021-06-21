99fee5557c687f0dd2f9175d4430b80ee1024b7d
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-06-21 10:54:27 +0200

    Updates

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ export * from './elements/create-profile-form';
 export * from './elements/search-agent';
 export * from './elements/profile-prompt';
 export * from './elements/list-profiles';
+export * from './elements/holo-identicon';
 export * from './profiles.service';
 export * from './profiles.store';
 //# sourceMappingURL=index.js.map
