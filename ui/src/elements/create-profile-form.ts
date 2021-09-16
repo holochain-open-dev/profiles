90b0a00b2a23c815bb162dc8489ded7b94fb012a
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-09-16 20:43:07 +0200

    Fixed px

--- ui/src/elements/create-profile-form.ts
+++ ui/src/elements/create-profile-form.ts
@@ -100,8 +100,8 @@ export class CreateProfileForm extends ScopedElementsMixin(LitElement) {
 
   // Crop the image and return a base64 bytes string of its content
   resizeAndExport(img: HTMLImageElement) {
-    const MAX_WIDTH = 50;
-    const MAX_HEIGHT = 50;
+    const MAX_WIDTH = 300;
+    const MAX_HEIGHT = 300;
 
     let width = img.width;
     let height = img.height;
