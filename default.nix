9c24763e26dac761f96e2420e05441aa577c211a
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-09-29 23:16:48 +0200

    Upgraded to hdk 108

--- default.nix
+++ default.nix
@@ -12,17 +12,18 @@ let
     holochainVersionId = "custom";
 
     holochainVersion = {
-      rev = "a1206a694fe3b521440fe633db99a50b8255c1b2";
-      sha256 = "sha256:0qdjjkqw3xlg8g686gvn509a9rv4kc6qfw07hypzc0fksix9d4iz";
-      cargoSha256 = "sha256:175b76j31sls0gj08imchwnk7n4ylsxlc1bm58zrhfmq62hcchb1";
+      rev = "holochain-0.0.108";
+      sha256 = "1p9rqd2d2wlyzc214ia93b1f18fgqspmza863q4hrz9ba6xigzjs";
+      cargoSha256 = "0p4m8ckbd7v411wgh14p0iz4dwi84i3cha5m1zgnqlln0wkqsb0f";
       bins = {
         holochain = "holochain";
         hc = "hc";
+        kitsune-p2p-proxy = "kitsune_p2p/proxy";
       };
 
       lairKeystoreHashes = {
-        sha256 = "sha256:0khg5w5fgdp1sg22vqyzsb2ri7znbxiwl7vr2zx6bwn744wy2cyv";
-        cargoSha256 = "sha256:1lm8vrxh7fw7gcir9lq85frfd0rdcca9p7883nikjfbn21ac4sn4";
+        sha256 = "0khg5w5fgdp1sg22vqyzsb2ri7znbxiwl7vr2zx6bwn744wy2cyv";
+        cargoSha256 = "1lm8vrxh7fw7gcir9lq85frfd0rdcca9p7883nikjfbn21ac4sn4";
       };
     };
   };
