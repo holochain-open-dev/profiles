02828f2fcc40001aedfeac79b8b8770ba4af9a8a
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-10-20 18:01:55 +0200

    Updates

--- dist/elements/agent-avatar.js
+++ dist/elements/agent-avatar.js
@@ -25,13 +25,13 @@ export class AgentAvatar extends ScopedElementsMixin(LitElement) {
         .hash=${this.agentPubKey}
         .size=${this.size}
       ></holo-identicon>`;
-        /*  if (this._profile.value)
-           return html`
-             <sl-avatar
-               .image=${this._profile.value.fields.avatar}
-               style="--size: ${this.size}px;"
-             ></sl-avatar>
-           `; */
+        if (this._profile.value)
+            return html `
+        <sl-avatar
+          .image=${this._profile.value.fields.avatar}
+          style="--size: ${this.size}px;"
+        ></sl-avatar>
+      `;
         return html `<sl-skeleton
       effect="pulse"
       style="height: ${this.size}px; width: ${this.size}px"
@@ -41,7 +41,7 @@ export class AgentAvatar extends ScopedElementsMixin(LitElement) {
         return {
             'holo-identicon': HoloIdenticon,
             'sl-avatar': SlAvatar,
-            'sl-skeleton': SlSkeleton
+            'sl-skeleton': SlSkeleton,
         };
     }
 }
