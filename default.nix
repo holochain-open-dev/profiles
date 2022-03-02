303de9a49f360fa464ddc2c86ff0bc2bfba271d2
Guillem Cordoba <guillem.cordoba@gmail.com>
2022-03-02 14:17:48 +0100

    Bumped hdk

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
-  holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/9ddfc68aaf665f13e674a5382946f2e262538abe.tar.gz";
+  holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/17bae6b00693f47a09456dec5e42dc54c840a25d.tar.gz";
   holonix = import (holonixPath) {
-    holochainVersionId = "v0_0_125";
+    holochainVersionId = "v0_0_126";
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
