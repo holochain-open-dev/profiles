f864aca5e42d643c06ee0623f9f31c74dbf4ef90
Guillem Cordoba <guillem.cordoba@gmail.com>
2022-03-08 15:24:30 +0100

    Updated to hdk 124

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
   holonixPath = builtins.fetchTarball "https://github.com/holochain/holonix/archive/f23119746d05fda8c62fd0e8b3874335b0258447.tar.gz";
   holonix = import (holonixPath) {
-    holochainVersionId = "v0_0_127";
+    holochainVersionId = "v0_0_128";
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
