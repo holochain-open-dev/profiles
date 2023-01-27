97d92da9de54e6b5937b2c29569f1c4a509fbf5e
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-01-27 16:19:17 +0100

    Updates

---
+++ profile-list-item-skeleton-stories.bf48db44.iframe.bundle.js
@@ -0,0 +1,20 @@
+(self.webpackChunk_holochain_open_dev_profiles_dev=self.webpackChunk_holochain_open_dev_profiles_dev||[]).push([[990],{"./stories/profile-list-item-skeleton.stories.js":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{"use strict";__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Demo:()=>Demo,__namedExportsOrder:()=>__namedExportsOrder,default:()=>profile_list_item_skeleton_stories});var lit_html=__webpack_require__("./node_modules/lit-html/lit-html.js"),tslib_es6=__webpack_require__("./node_modules/tslib/tslib.es6.js"),decorators=__webpack_require__("./node_modules/lit/decorators.js"),profile_list_item_skeleton=__webpack_require__("./ui/dist/elements/profile-list-item-skeleton.js");let PLIS=class PLIS extends profile_list_item_skeleton.q{};PLIS=(0,tslib_es6.gn)([(0,decorators.Mo)("profile-list-item-skeleton")],PLIS);const profile_list_item_skeleton_stories={title:"Frontend/Elements/profile-list-item-skeleton",tags:["autodocs"],component:"profile-list-item-skeleton",render:args=>lit_html.dy`
+      <profile-list-item-skeleton />
+      <profile-list-item-skeleton />
+      <profile-list-item-skeleton />
+    `},Demo={};Demo.parameters={...Demo.parameters,docs:{...Demo.parameters?.docs,source:{originalSource:"{}",...Demo.parameters?.docs?.source}}};const __namedExportsOrder=["Demo"]},"./ui/dist/elements/profile-list-item-skeleton.js":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{"use strict";__webpack_require__.d(__webpack_exports__,{q:()=>ProfileListItemSkeleton});var lit__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__("./node_modules/lit/index.js"),_open_wc_scoped_elements__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__("./node_modules/@open-wc/scoped-elements/index.js"),_scoped_elements_shoelace__WEBPACK_IMPORTED_MODULE_2__=__webpack_require__("./node_modules/@scoped-elements/shoelace/dist/index.js"),_holochain_open_dev_elements__WEBPACK_IMPORTED_MODULE_3__=__webpack_require__("./node_modules/@holochain-open-dev/elements/dist/index.js");class ProfileListItemSkeleton extends((0,_open_wc_scoped_elements__WEBPACK_IMPORTED_MODULE_1__.f)(lit__WEBPACK_IMPORTED_MODULE_0__.oi)){render(){return lit__WEBPACK_IMPORTED_MODULE_0__.dy`<div class="row" style="align-items: center; width: 150px">
+      <sl-skeleton
+        effect="sheen"
+        style="height: 32px; width: 32px; border-radius: 50%; margin: 8px"
+      ></sl-skeleton
+      ><sl-skeleton
+        effect="sheen"
+        style="flex: 1; margin: 8px; border-radius: 12px"
+      >
+      </sl-skeleton>
+    </div>`}static get styles(){return[_holochain_open_dev_elements__WEBPACK_IMPORTED_MODULE_3__.FG,lit__WEBPACK_IMPORTED_MODULE_0__.iv`
+        :host {
+          display: flex;
+        }
+      `]}static get scopedElements(){return{"sl-skeleton":_scoped_elements_shoelace__WEBPACK_IMPORTED_MODULE_2__.pH}}}},"?cdf8":()=>{}}]);
+//# sourceMappingURL=profile-list-item-skeleton-stories.bf48db44.iframe.bundle.js.map
