540d63d147db6f274d507938c3ea8655560a0bf9
guillem.cordoba <guillem.cordoba@gmail.com>
2024-02-15 12:05:17 +0100

    Tests passing again

--- crates/integrity/src/lib.rs
+++ crates/integrity/src/lib.rs
@@ -24,7 +24,7 @@ pub struct Profile {
 
 #[derive(Serialize, Deserialize)]
 #[serde(tag = "type")]
-#[hdk_entry_defs]
+#[hdk_entry_types]
 #[unit_enum(UnitEntryTypes)]
 pub enum EntryTypes {
     Profile(Profile),
