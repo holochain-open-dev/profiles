cc68c2eae854d95654cc38094397aeace180b82d
guillem.cordoba <guillem.cordoba@gmail.com>
2024-03-12 17:23:43 +0100

    WIP

--- ui/ui.nix
+++ ui/ui.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ inputs, ... }:
 
 {
   perSystem =
@@ -11,34 +11,11 @@
     , self'
     , ...
     }: {
-      packages.profiles_ui =
-        let 
-          rootPackageJson = builtins.fromJSON (builtins.readFile ../package.json);
-          packageJson = builtins.fromJSON (builtins.readFile ./package.json);
-          builtNodeModules = pkgs.buildNpmPackage {
-            pname = packageJson.name;
-            version = "0.0.0";
-            src = ../.;
-            npmWorkspace = "ui";
-            npmDeps = pkgs.importNpmLock {
-              npmRoot = ../.;
-            };
-            npmConfigHook = pkgs.importNpmLock.npmConfigHook;
-          };
-        in
-          pkgs.runCommandLocal packageJson.name {} ''
-            set -e
-            cd ${builtNodeModules}/lib/node_modules/${rootPackageJson.name}
-            mkdir $out
-            ${pkgs.rsync}/bin/rsync -aP --exclude=node_modules ./* $out
-          '';
-      # packages.deps = builtins.trace (pkgs.importNpmLock {
-      #   npmRoot = ../.;
-      #   # packageLock = ./package-lock.json;
-      # }) (pkgs.importNpmLock {
-      #   npmRoot = ../.;
-      #   # packageLock = ./package-lock.json;
-      # });
+      packages.profiles_ui = inputs.hcUtils.lib.npmPackage {
+        rootPath = ../.;
+        workspacePath = ./.;
+        inherit system;
+      };
   	};
 }
 
