30fdec0550b351a4aef7f514f4aa7af2f3be74a4
guillem.cordoba <guillem.cordoba@gmail.com>
2024-03-12 13:42:10 +0100

    WIP

--- ui/ui.nix
+++ ui/ui.nix
@@ -11,21 +11,7 @@
     , self'
     , ...
     }: {
-      packages.profiles_ui =
-        let 
-          packageJson = builtins.fromJSON (builtins.readFile ./package.json);
-        in
-          pkgs.buildNpmPackage {
-            pname = packageJson.name;
-            version = packageJson.version;
-            src = ../.;
-            npmWorkspace = packageJson.name;
-            npmDeps = pkgs.importNpmLock {
-              npmRoot = ../.;
-              # packageLock = ./package-lock.json;
-            };
-            npmDepsHash = "sha256-ZM3FT9zPS1rE2ke5i1X0Vhmoe3VtXC+E9NNtla1TIIU=";
-          };
+
   	};
 }
 
