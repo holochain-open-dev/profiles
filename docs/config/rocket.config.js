77ec69efc9214ce37cfe8cc532c91401471883a9
Guillem Córdoba <guillem.cordoba@gmail.com>
2024-03-20 16:00:22 +0100

    vitepress

--- docs/config/rocket.config.js
+++
@@ -1,7 +0,0 @@
-import { rocketLaunch } from '@rocket/launch';
-import { presetRocketSearch } from '@rocket/search';
-
-export default /** @type {import('@rocket/cli').RocketCliOptions} */ ({
-  absoluteBaseUrl: 'http://localhost:8080',
-  presets: [rocketLaunch(), presetRocketSearch()],
-});
--- docs/site/pages/40--blog/recursive.data.js
+++
@@ -1,6 +0,0 @@
-import { LayoutBlogPost } from '@rocket/launch';
-import { layoutData } from '#src/layouts/layoutData.js';
-
-export const layout = new LayoutBlogPost({
-  ...layoutData,
-});
