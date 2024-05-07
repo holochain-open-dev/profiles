1e6c553edef5b7070ea46496dc19c7a789161db5
Eric Harris-Braun <eric@harris-braun.com>
2024-05-06 22:07:42 -0400

    fixes for updated ui/elements

--- ui/src/elements/profile-prompt.ts
+++ ui/src/elements/profile-prompt.ts
@@ -9,7 +9,6 @@ import {
 } from "@holochain-open-dev/stores";
 import {
   sharedStyles,
-  withSpinnerAndDisplayError,
 } from "@holochain-open-dev/elements";
 
 import "@shoelace-style/shoelace/dist/components/spinner/spinner.js";
@@ -78,4 +77,46 @@ export class ProfilePrompt extends LitElement {
       `,
     ];
   }
+}
+
+export function withSpinnerAndDisplayError<T>(renderers: {
+  complete: (value: T) => TemplateResult;
+  error: ((error: any) => TemplateResult) | { label: string; tooltip: boolean };
+}) {
+  return renderAsyncStatus({
+    pending: () => html`<div
+      style="display: flex; flex-direction: column; align-items: center; justify-content: center; flex: 1;"
+    >
+      <sl-spinner style="font-size: 2rem;"></sl-spinner>
+    </div>`,
+    error: (e: any) =>
+      typeof renderers.error === "function"
+        ? renderers.error(e)
+        : html`<display-error
+            .headline=${renderers.error?.label}
+            .tooltip=${renderers.error?.tooltip}
+            .error=${e}
+          ></display-error> `,
+    complete: renderers.complete,
+  });
+}
+
+/**
+ * Renders the given AsyncStatus with the given renderers
+ */
+export function renderAsyncStatus<T>(renderers: {
+  complete: (value: T) => TemplateResult;
+  error: (error: any) => TemplateResult;
+  pending: () => TemplateResult;
+}) {
+  return (status: AsyncStatus<T>) => {
+    switch (status.status) {
+      case "complete":
+        return renderers.complete(status.value);
+      case "error":
+        return renderers.error(status.error);
+      default:
+        return renderers.pending();
+    }
+  };
 }
