a4731cf3c007d2cb354442f64e35a56c6a515369
guillem.cordoba <guillem.cordoba@gmail.com>
2024-05-08 12:58:14 +0200

    Fixed search-agent

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   description = "Template for Holochain app development";
-  
+
   inputs = {
     nixpkgs.follows = "holochain/nixpkgs";
 
-    versions.url = "github:holochain/holochain?dir=versions/weekly";
+    versions.url = "github:holochain/holochain?dir=versions/0_3_rc";
 
     holochain = {
       url = "github:holochain/holochain";
@@ -14,57 +14,41 @@
   };
 
   nixConfig = {
-		extra-substituters = [
-	    "https://holochain-open-dev.cachix.org"
-	  ];	
-		extra-trusted-public-keys = [
-			"holochain-open-dev.cachix.org-1:3Tr+9in6uo44Ga7qiuRIfOTFXog+2+YbyhwI/Z6Cp4U="
-	  ];
-	};
+    extra-substituters = [ "https://holochain-open-dev.cachix.org" ];
+    extra-trusted-public-keys = [
+      "holochain-open-dev.cachix.org-1:3Tr+9in6uo44Ga7qiuRIfOTFXog+2+YbyhwI/Z6Cp4U="
+    ];
+  };
 
-  outputs = inputs @ { ... }:
-    inputs.holochain.inputs.flake-parts.lib.mkFlake
-      {
-        inherit inputs;
-        specialArgs.rootPath = ./.;
-      }
-      {
-        imports = [
-          ./crates/coordinator/zome.nix
-          ./crates/integrity/zome.nix
-        ];
+  outputs = inputs@{ ... }:
+    inputs.holochain.inputs.flake-parts.lib.mkFlake {
+      inherit inputs;
+      specialArgs.rootPath = ./.;
+    } {
+      imports = [ ./crates/coordinator/zome.nix ./crates/integrity/zome.nix ];
 
-        systems = builtins.attrNames inputs.holochain.devShells;
-        perSystem =
-          { inputs'
-          , config
-          , pkgs
-          , system
-          , lib
-          , ...
-          }: {
-            devShells.default = pkgs.mkShell {
-              inputsFrom = [ inputs'.holochain.devShells.holonix ];
-              packages = with pkgs; [
-                nodejs_20
-              ];
-            };
+      systems = builtins.attrNames inputs.holochain.devShells;
+      perSystem = { inputs', config, pkgs, system, lib, ... }: {
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ inputs'.holochain.devShells.holonix ];
+          packages = with pkgs; [ nodejs_20 ];
+        };
 
-            packages.scaffold = pkgs.symlinkJoin {
-              name = "scaffold-remote-zome";
-              paths = [ inputs'.hc-infra.packages.scaffold-remote-zome ];
-              buildInputs = [ pkgs.makeWrapper ];
-              postBuild = ''
-                wrapProgram $out/bin/scaffold-remote-zome \
-                  --add-flags "profiles \
-                    --integrity-zome-name profiles_integrity \
-                    --coordinator-zome-name profiles \
-                    --remote-zome-git-url github:holochain-open-dev/profiles \
-                    --remote-zome-git-branch nixify \
-                    --remote-npm-package-name @holochain-open-dev/profiles \
-                    --remote-npm-package-path ui"
-              '';
-            };
-          };
+        packages.scaffold = pkgs.symlinkJoin {
+          name = "scaffold-remote-zome";
+          paths = [ inputs'.hc-infra.packages.scaffold-remote-zome ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/scaffold-remote-zome \
+              --add-flags "profiles \
+                --integrity-zome-name profiles_integrity \
+                --coordinator-zome-name profiles \
+                --remote-zome-git-url github:holochain-open-dev/profiles \
+                --remote-zome-git-branch nixify \
+                --remote-npm-package-name @holochain-open-dev/profiles \
+                --remote-npm-package-path ui"
+          '';
+        };
       };
+    };
 }
--- ui/src/profiles-client.ts
+++ ui/src/profiles-client.ts
@@ -1,75 +1,77 @@
-import { EntryRecord, ZomeClient } from "@holochain-open-dev/utils";
+import { EntryRecord, ZomeClient } from '@holochain-open-dev/utils';
 import {
-  AgentPubKey,
-  Record,
-  AppClient,
-  RoleName,
-  Link,
-} from "@holochain/client";
-import { Profile, ProfilesSignal } from "./types";
+	AgentPubKey,
+	AppClient,
+	Link,
+	Record,
+	RoleName,
+} from '@holochain/client';
+
+import { Profile, ProfilesSignal } from './types';
 
 export class ProfilesClient extends ZomeClient<ProfilesSignal> {
-  constructor(
-    public client: AppClient,
-    public roleName: RoleName,
-    public zomeName = "profiles"
-  ) {
-    super(client as any, roleName, zomeName);
-  }
+	constructor(
+		public client: AppClient,
+		public roleName: RoleName,
+		public zomeName = 'profiles',
+	) {
+		super(client as any, roleName, zomeName);
+	}
 
-  /**
-   * Get the profile for the given agent, if they have created it
-   *
-   * @param agentPubKey the agent to get the profile for
-   * @returns the profile of the agent, if they have created one
-   */
-  async getAgentProfile(
-    agentPubKey: AgentPubKey
-  ): Promise<EntryRecord<Profile> | undefined> {
-    const record: Record | undefined = await this.callZome(
-      "get_agent_profile",
-      agentPubKey
-    );
+	/**
+	 * Get the profile for the given agent, if they have created it
+	 *
+	 * @param agentPubKey the agent to get the profile for
+	 * @returns the profile of the agent, if they have created one
+	 */
+	async getAgentProfile(
+		agentPubKey: AgentPubKey,
+	): Promise<EntryRecord<Profile> | undefined> {
+		const record: Record | undefined = await this.callZome(
+			'get_agent_profile',
+			agentPubKey,
+		);
 
-    return record ? new EntryRecord(record) : undefined;
-  }
+		return record ? new EntryRecord(record) : undefined;
+	}
 
-  /**
-   * Search profiles that start with nicknameFilter
-   *
-   * @param nicknameFilter must be of at least 3 characters
-   * @returns the agents with the nickname starting with nicknameFilter
-   */
-  async searchAgents(nicknameFilter: string): Promise<AgentPubKey[]> {
-    return this.callZome("search_agents", nicknameFilter);
-  }
+	/**
+	 * Search profiles that start with nicknameFilter
+	 *
+	 * @param nicknameFilter must be of at least 3 characters
+	 * @returns the agents with the nickname starting with nicknameFilter
+	 */
+	async searchAgents(nicknameFilter: string): Promise<AgentPubKey[]> {
+		console.log('searchagents', nicknameFilter);
+		return this.callZome('search_agents', nicknameFilter);
+	}
 
-  /**
-   * Get all the agents in the DHT that have created a profile
-   *
-   * @returns the agent public keys of all agents that have created a profile
-   */
-  async getAgentsWithProfile(): Promise<Link[]> {
-    return this.callZome("get_agents_with_profile", null);
-  }
+	/**
+	 * Get all the agents in the DHT that have created a profile
+	 *
+	 * @returns the agent public keys of all agents that have created a profile
+	 */
+	async getAgentsWithProfile(): Promise<Link[]> {
+		return this.callZome('get_agents_with_profile', null);
+	}
 
-  /**
-   * Create my profile
-   *
-   * @param profile the profile to create
-   */
-  async createProfile(profile: Profile): Promise<EntryRecord<Profile>> {
-    const record: Record = await this.callZome("create_profile", profile);
-    return new EntryRecord(record);
-  }
+	/**
+	 * Create my profile
+	 *
+	 * @param profile the profile to create
+	 */
+	async createProfile(profile: Profile): Promise<EntryRecord<Profile>> {
+		const record: Record = await this.callZome('create_profile', profile);
+		return new EntryRecord(record);
+	}
 
-  /**
-   * Update my profile
-   *
-   * @param profile the profile to create
-   */
-  async updateProfile(profile: Profile): Promise<EntryRecord<Profile>> {
-    const record: Record = await this.callZome("update_profile", profile);
-    return new EntryRecord(record);
-  }
+	/**
+	 * Update my profile
+	 *
+	 * @param profile the profile to create
+	 */
+	async updateProfile(profile: Profile): Promise<EntryRecord<Profile>> {
+		const record: Record = await this.callZome('update_profile', profile);
+		return new EntryRecord(record);
+	}
 }
