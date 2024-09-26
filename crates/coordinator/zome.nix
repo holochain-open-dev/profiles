719e10d9cf6d4f97b587175fecaa7b6ee3bc836e
guillem.cordoba <guillem.cordoba@gmail.com>
2024-09-26 14:18:31 +0200

    Updated hc-infra

--- crates/coordinator/zome.nix
+++ crates/coordinator/zome.nix
@@ -2,16 +2,14 @@
 
 {
   perSystem = { inputs', config, pkgs, system, lib, options, self', ... }: {
-    packages.profiles = inputs.hc-infra.outputs.lib.rustZome {
-      inherit system;
+    packages.profiles = inputs.hc-infra.outputs.builders.${system}.rustZome {
       workspacePath = inputs.self.outPath;
       crateCargoToml = ./Cargo.toml;
     };
 
-    checks.profiles = inputs.hc-infra.outputs.lib.sweettest {
-      inherit system;
+    checks.profiles = inputs.hc-infra.outputs.builders.${system}.sweettest {
       workspacePath = inputs.self.outPath;
-      dna = inputs.hc-infra.outputs.lib.dna {
+      dna = inputs.hc-infra.outputs.builders.${system}.dna {
         dnaManifest = builtins.toFile "dna.yaml" ''
           ---
           manifest_version: "1"
@@ -30,8 +28,10 @@
                   - name: profiles_integrity
                 dylib: ~
         '';
-        zomes = inputs.hc-infra.outputs.lib.filterZomes self'.packages;
-        inherit system;
+        zomes = {
+          profiles = self'.packages.profiles;
+          profiles_integrity = self'.packages.profiles_integrity;
+        };
       };
       crateCargoToml = ./Cargo.toml;
     };
--- crates/integrity/zome.nix
+++ crates/integrity/zome.nix
@@ -2,11 +2,11 @@
 
 {
   perSystem = { inputs', config, pkgs, system, lib, options, ... }: {
-    packages.profiles_integrity = inputs.hc-infra.outputs.lib.rustZome {
-      inherit system;
-      workspacePath = inputs.self.outPath;
-      crateCargoToml = ./Cargo.toml;
-    };
+    packages.profiles_integrity =
+      inputs.hc-infra.outputs.builders.${system}.rustZome {
+        workspacePath = inputs.self.outPath;
+        crateCargoToml = ./Cargo.toml;
+      };
   };
 }
 
